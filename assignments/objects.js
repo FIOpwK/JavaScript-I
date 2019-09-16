8d4dcdb0914e3ca7f32b6faea2d7dd2744f66b26
fiopwk <flopwiki@gmail.com>
2019-09-16 17:55:11 -0400

    objects: created intern database

--- assignments/objects.js
+++ assignments/objects.js
@@ -3,12 +3,6 @@
 // ==== Challenge 1: Writing Objects ==== 
 // HR needs some information on the new interns put into a database.  Given an id, email, first name, and gender. Create an object for each person in the company list:
 
-// 1, mmelloy0@psu.edu, Mitzi, F
-// 2, kdiben1@tinypic.com, Kennan, M
-// 3, kmummery2@wikimedia.org, Keven, M
-// 4, gmartinson3@illinois.edu, Gannie, M
-// 5, adaine5@samsung.com, Antonietta, F
-
 // Example format of an intern object: 1, examples@you.edu, Example, F
 const example = {
   id: 0,
@@ -18,7 +12,34 @@ const example = {
 }
 
 // Write your intern objects here:
+const interns = {
+id: 1,
+email: 'mmelloy0@psu.edu',
+"first name": 'Mitzi',
+gender: 'F',
 
+id: 2,
+email: 'kdiben1@tinypic.com',
+"first name": 'Kennan',
+gender 'M',
+
+id: 3,
+email: 'kmummery2@wikimedia.org',
+first_name: 'Keven',
+gender: 'M',
+
+id: 4,
+email: 'gmartinson3@illinois.edu',
+first_name: 'Gannie',
+gender: 'M',
+
+
+id: 5,
+email: 'adaine5@samsung.com',
+first_name: 'Antonietta',
+gender: 'F'
+
+}
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
